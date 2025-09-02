0ddf2ee611885d7772a855ed10748e94f9add2c9
Álvaro Filipe <152521789+AlvaroF1lip3@users.noreply.github.com>
2025-09-01 22:43:56 -0300

    Add files via upload

---
+++ Lista-01/Exercicios-Resolvidos/Q01/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+public class Principal {
+	    public static void main(String[] args) {
+	    	
+	        int n1 = 5;
+	        int n2 = 10;
+	        int n3 = 7;
+	        int n4 = 3;
+	        
+	        
+	        int soma = n1 + n2 + n3 + n4;
+	        
+	        
+	        System.out.println("[Soma] = " + soma + " 👈(ﾟヮﾟ👈)");
+	    }
+	}
